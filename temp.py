c557a2fa55fce48466ec76c5dc39fb12700ade82
Rahul Mali <58736566+RahulMAli029@users.noreply.github.com>
2021-03-19 22:42:23 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -23,6 +23,7 @@ data.reset_index(inplace=True)
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 app.title = 'COVID-19: Prediction And Analysis Of Spreading Rate'
 
 
